9be1b2d5b6bbc34c3ba8e31f76fe026d031f5bd9
lecosh <artem_vorobey_2001@mail.ru>
2024-02-01 19:31:18 +0300

    Bot 1.3

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ from constants import *
 
 bot = telebot.TeleBot(API_KEY)
 study_schedule = ""
+logger = logging.getLogger()
+logging.basicConfig(filename=LOG_FILE,filemode="a", level=logging.INFO, format='%(asctime)-15s %(levelname)-2s %(message)s')
+logger=logging.getLogger(__name__)
 
 def get_tomorrow_weekday():
     day = datetime.now().weekday() % 6
@@ -10,43 +13,26 @@ def get_tomorrow_weekday():
     return day
 
 def get_gym():
-    day = get_tomorrow_weekday()
-    if day == 0:
-        return gym_schedule[0]
-    elif day == 3:
-        return gym_schedule[1]
-    elif day == 5:
-        return gym_schedule[2]
-    else:
-        return gym_schedule[3]
+    next_day = get_tomorrow_weekday()
+    return gym_schedule[next_day]
 
 def get_job():
-    work_time = get_tomorrow_weekday()
-    if work_time > 3:
-        return "Work - Chill"
-    else:
-        return work_schedule[work_time]
+    next_day = get_tomorrow_weekday()
+    return work_schedule[next_day]
 
-def get_day(href):
+def get_study_date(href):
     return href.find("div", "schedule__date").text
 
-def get_subjects(href):
-    subj_arr = []
-    all_subjects = href.find_all("div", "lesson__subject")
-    for subject in all_subjects:
-        subj_arr.append(subject.text)
-    return subj_arr
-
-def get_place(href):
-    place_arr = []
-    all_places = href.find_all("div", "lesson__places")
-    for place in all_places:
-        place_arr.append(place.text)
-    return place_arr
+def get_class_data(href, div_class):
+    class_data_arr = []
+    class_data = href.find_all("div", div_class)
+    for data in class_data:
+        class_data_arr.append(data.text)
+    return class_data_arr
 
 def get_xml(url):
-    logger.info("Requesting schedule...")
     try:
+        logger.info("Requesting schedule...")
         response = requests.get(url)
     except Exception as e:
         logger.error("Bad request:", e)
@@ -62,48 +48,37 @@ def get_xml(url):
             
 def get_url():
     if datetime.now().weekday() == 6:
-        current_day = datetime.now().today() + timedelta(days=1)
+        monday_date = datetime.now().today() + timedelta(days=1)
     else:
-        current_day = datetime.now().today() - timedelta(days=datetime.now().weekday())
-    logger.info(current_day)
-    url = URL_BASE + str(current_day.year) + "-" + str(current_day.month) + "-" + str(current_day.day)
+        monday_date = datetime.now().today() - timedelta(days=datetime.now().weekday())
+    url = URL_BASE + str(current_day.year) + "-" + str(monday_date.month) + "-" + str(monday_date.day)
     get_xml(url)
 
 def format_output(day):
     global study_schedule
-    i = 0
-    date = get_day(day)
-    subjects = get_subjects(day)
-    places = get_place(day)
+    date = get_study_date(day)
+    subjects = get_class_data(day, "lesson__subject")
+    places = get_class_data(day, "lesson__places")
     study_schedule = date + "\n"
-    for subj in subjects:
-        study_schedule += subj
-        study_schedule += "\t" + places[i]
+    for subject, place in zip(subjects, places):
+        study_schedule += subject
         study_schedule += "\n"
-        i += 1
+        study_schedule += place
+        study_schedule += "\n"
+        
+    if study_schedule == "":
+        study_schedule = "Polytech - chill"
+    bot.send_message(chat_id, "Your schedule for tommorow is:\n" + \
+                              study_schedule + get_job()+ "\n" + get_gym())
+    study_schedule = ""
+    time.sleep(60)
 
 def job():
     get_url()
-    return_schedule()
-
-def return_schedule():
-    global study_schedule
-    if study_schedule == "":
-        bot.send_message(chat_id, "Your schedule for tommorow is:\n" + "\t\t\tPolytech - chill" + "\n" + "\t\t\t" + get_job()+ "\n" + "\t\t\t" + get_gym())
-    else:
-        bot.send_message(chat_id, "Your schedule for tommorow is:\n" + "\t\t\t" + study_schedule + "\n" + "\t\t\t" + get_job()+ "\n" + "\t\t\t" + get_gym())
-    time.sleep(60)
-
-@bot.message_handler(commands=["start"])
-def start(m, res=False):
-    bot.send_message(m.chat.id, 'Hello, text something to start bot')
 
 if __name__ == "__main__":
-    logger = logging.getLogger()
-    logging.basicConfig(filename=LOG_FILE,filemode="a", level=logging.INFO, format='%(asctime)-15s %(levelname)-2s %(message)s')
-    logger=logging.getLogger(__name__)
     bot.send_message(chat_id, "Bot has been started.")
-    schedule.every().day.at("11:43").do(job)
+    schedule.every().day.at("19:25").do(job)
     logger.info("Bot has been started...")
     threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
     while True:
